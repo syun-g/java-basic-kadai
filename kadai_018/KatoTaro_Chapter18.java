65b1308369a667f14ac8d88df2a9b1b15becad18
syun-g <162730686+syun-g@users.noreply.github.com>
2024-06-21 23:00:37 +0900

    kadai_018を追加

---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	
+	//名前を設定
+      public void setGivenName() {
+       givenName = "太郎";
+     }
+
+	//抽象メソッド
+	  public void eachIntroduce() {
+		
+	   System.out.println("Javaが得意です");
+      }
+
+}
+
